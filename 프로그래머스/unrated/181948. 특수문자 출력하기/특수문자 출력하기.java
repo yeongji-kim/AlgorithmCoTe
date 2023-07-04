8d0eea9fbab3f02124d05609248b529c208fea75
김영지(Yeongji-Kim) <75402057+yeongji-kim@users.noreply.github.com>
2023-07-04 23:10:00 +0900

    [unrated] Title: 특수문자 출력하기, Time: 141.53 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String solution = "!@#$%^&*(\\'\"<>?:;";
+       System.out.print(solution) ;
+    }
+}
