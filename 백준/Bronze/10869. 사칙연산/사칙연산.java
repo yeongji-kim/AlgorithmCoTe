a3264e556038d1ef37aad56f3de4f716307dd991
김영지(Yeongji-Kim) <75402057+yeongji-kim@users.noreply.github.com>
2023-09-11 23:45:49 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+// 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+import java.util.*;
+class Main {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
